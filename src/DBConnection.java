c6feeb51f6322f3f2d3032fa6446647dd8ce4f83
Abrar Ali <abrarali.se@gmail.com>
2025-05-19 15:49:46 +0500

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/cartsydb?useSSL=true&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASS = "abrarali";
+    private static final String PASS = "";
 
     public static Connection getConnection() {
         try {
